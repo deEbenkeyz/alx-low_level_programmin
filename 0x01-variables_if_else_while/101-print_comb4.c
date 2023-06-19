2e0c600aad53edffcc055b600c08d150b43f2bc9
Ebenezer Ohene-Adutwum <eben.oadutwum@gmail.com>
2023-06-19 01:01:03 -0700

    Advanced tasks

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Prints combinations of numbers
+ *
+ * Return: Always (success)
+ */
+int main(void)
+{
+	int value_01, value_02, value_03;
+
+	for(value_01='0'; value_01<='9'; value_01++){
+	  for(value_02='0'; value_02<='9'; value_02++){
+	    for(value_03='0'; value_03<='9'; value_03++){
+	    
+
+	      if((value_01 < value_02) && (value_02 < value_03)){
+		putchar(value_01);
+		putchar(value_02);
+		putchar(value_03);
+
+		if(value_01 != '7' || (value_01 != '7' && value_02 != '8' && value_03 != '9')){
+		  putchar(',');
+		  putchar(' ');
+		}
+	      }
+	    }
+	  }
+	}
+	
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two two-digit numbers,
+ *        ranging from 0-99, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int value_1, value_2;
+
+	for (value_1 = 0; value_1 <= 98; value_1++) {
+	  for (value_2 = value_1 + 1; value_2 <= 99; value_2++) {
+	    putchar((value_1 / 10) + '0');
+            putchar((value_1 % 10) + '0');
+            putchar(' ');
+            putchar((value_2 / 10) + '0');
+            putchar((value_2 % 10) + '0');
+
+            if (value_1 == 98 && value_2 == 99)
+                continue;
+
+            putchar(',');
+            putchar(' ');
+	  }
+	}
+	putchar('\n');
+
+	return (0);
+}
