5c756d8df31b222c6ff039038105b4ca72136a8c
Ebenezer Ohene-Adutwum <eben.oadutwum@gmail.com>
2023-06-19 01:09:25 -0700

    Betty issues - fix 2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,6 +7,7 @@
 int main(void)
 {
 	int value_one, value_two;
+
 	for(value_one='0'; value_one<='9'; value_one++){
 	  for(value_two='0'; value_two<='9'; value_two++){
 	    if(value_one < value_two){
