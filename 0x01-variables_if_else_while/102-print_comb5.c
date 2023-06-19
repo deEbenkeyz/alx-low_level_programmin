3d9e56385c078ba65e8911cf13f59205a006161c
Ebenezer Ohene-Adutwum <eben.oadutwum@gmail.com>
2023-06-19 01:58:10 -0700

    Betty issues - fix 4

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,22 +11,24 @@ int main(void)
 {
 	int value_1, value_2;
 
-	for (value_1 = 0; value_1 <= 98; value_1++) {
-	  for (value_2 = value_1 + 1; value_2 <= 99; value_2++) {
-	    putchar((value_1 / 10) + '0');
-            putchar((value_1 % 10) + '0');
-            putchar(' ');
-            putchar((value_2 / 10) + '0');
-            putchar((value_2 % 10) + '0');
+	for (value_1 = 0; value_1 <= 98; value_1++)
+	{
+		for (value_2 = value_1 + 1; value_2 <= 99; value_2++)
+		{
+			putchar((value_1 / 10) + '0');
+			putchar((value_1 % 10) + '0');
+			putchar(' ');
+			putchar((value_2 / 10) + '0');
+			putchar((value_2 % 10) + '0');
 
-            if (value_1 == 98 && value_2 == 99)
-                continue;
+			if (value_1 == 98 && value_2 == 99)
+				continue;
 
-            putchar(',');
-            putchar(' ');
-	  }
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar('\n');
 
+	putchar('\n');
 	return (0);
 }
