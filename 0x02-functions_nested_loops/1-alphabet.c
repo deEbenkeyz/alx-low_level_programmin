efe8a1527283e72e08f02a421d4171ef35a554b3
Ebenezer Ohene-Adutwum <eben.oadutwum@gmail.com>
2023-06-20 12:32:43 -0700

    1-alphabet Project

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print the 26 english alphabet
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+
+	_putchar('\n');
+}
