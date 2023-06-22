ce6493cb476dbecdc9d48d791a13c07f3e3610cc
Ebenezer Ohene-Adutwum <eben.oadutwum@gmail.com>
2023-06-22 10:29:57 -0700

    1-isdigit.c project

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - function to check if value is a digit
+ *
+ * Return: Always 0.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
