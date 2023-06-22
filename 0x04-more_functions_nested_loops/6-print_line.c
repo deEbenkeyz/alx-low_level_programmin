2d9127e594e9e51711145c2a24d5862cac8d3511
Ebenezer Ohene-Adutwum <eben.oadutwum@gmail.com>
2023-06-22 15:03:21 -0700

    6-print_line.c Project

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - A function to print line
+ * @n: Value to be checked
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int line;
+
+		for (line = 0; line <= n; line++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
